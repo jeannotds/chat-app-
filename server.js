2984a2d693341ef51d31f5acd48749f248b6c6ce
jeannot diambu <diambujeannot@gmail.com>
2022-10-04 13:16:44 +0100

    utlisation de middleware

---
+++ server.js
@@ -0,0 +1,9 @@
+
+const app = require('./app')
+const http = require('http')
+
+
+app.set('port', process.env.PORT || 3000)
+const server = http.createServer(app)
+server.listen(process.env.POR || 3000)
+
