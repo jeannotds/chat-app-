f3508d22cc2d0d20d0b5693bf4256b5885a3b8bd
jeannot diambu <diambujeannot@gmail.com>
2022-10-04 16:03:31 +0100

    connexion à la base de données

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
+
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+
+
+
+  mongoose.connect('mongodb+srv://jeannotds:jeannot1997@cluster0.f0osgcn.mongodb.net/?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -9,7 +20,13 @@ app.use((req, res, next) => {
     next();
   });
 
-app.use('/api/stuff', (req, res, next) => {
+
+app.post('/api/stuff', (req, res, next) => {
+    console.log(req.body)
+    res.status(200).json({message : "Objet creer"})
+})
+
+ app.get('/api/stuff', (req, res, next) => {
     const stuff = [
         {
           _id: 'oeihfzeoi',
