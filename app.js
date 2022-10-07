6e9b045757e610d56b5338e06abd39a2598b55b4
jeannot diambu <diambujeannot@gmail.com>
2022-10-08 00:38:31 +0100

    Amelioration des routes

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 
 const express = require('express');
 const mongoose = require('mongoose');
-const Thing = require('./models/Thing')
 const bodyParser = require('body-parser')
+const routerRoutes = require('./routes/stuff')
 
   mongoose.connect('mongodb+srv://jeannotds:jeannot1997@cluster0.f0osgcn.mongodb.net/?retryWrites=true&w=majority',
   { useNewUrlParser: true,
@@ -21,42 +21,7 @@ app.use((req, res, next) => {
     next();
   });
 
+app.use('/api/stuff', routerRoutes)
 
-app.get('/api/stuff/:_id', (req, res, next) => {
-    Thing.findOne({_id: req.params.id}) // Pour trouver un seule objet
-    .then(thing => res.status(200).json(thing))
-    .catch(error => res.status(404).json(error))
-})
-
-app.get('/api/stuff', (req, res, next) => {
-  Thing.find() // Pour trouver tous les objets
-  .then(things => res.status(200).json(things))
-  .catch(error => res.status(400).json())
-});
-
-app.post('/api/stuff', (req, res, next) => {
-    // enlever le champ Id du corps de la requete parce qu'il sera generé auto.
-    // Retirer le champs Id avant de copier
-    delete req.body._id; 
-    const thing = new Thing({...req.body})
-    // Enregistre l'objet dans la BD et retourne une promesse
-    thing.save()
-    .then(res.status(201).json({message : "Objet enregistré !"})) // renvoyer une reponse
-    .catch(error => res.status(404).json(error)) // Recuperer l'error et envoyer un code 404
-})
-
-
-app.delete('/api/stuff/:id', (req, res, next) => {
-  Thing.deleteOne({ _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
-    .catch(error => res.status(400).json({ error }));
-});
-
-app.put('/api/stuff/:id', (req, res, next) => {
-  Thing.updateOne({ _id: req.params.id}, {...req.body, _id: req.params.id})
-  .then(() => res.status(200).json({ message :"Objet modifié" }))
-  .catch(error => res.status(404).json({ error }))
-
-})
 
 module.exports = app;
