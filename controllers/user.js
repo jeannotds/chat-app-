a21fb769e0566d56c9647f61054251d9961c18be
jeannot diambu <diambujeannot@gmail.com>
2022-10-14 12:46:10 +0100

    creation de token

--- controllers/user.js
+++ controllers/user.js
@@ -1,18 +1,9 @@
 
 const bcrypt = require('bcrypt')
 const User = require('../models/User')
+const jwt = require('jsonwebtoken') //jsonwebtoken
 
 //Enregistrement nouveau utilisateur
-// exports.signup = (req, res, next) => {
-//     bcrypt.hash(req.body.password, 10)
-//     .then( hash => { 
-//         const user = new User ({ email: req.body.email, password: hash});
-//         user.save()
-//         .then(() => res.status(201).json({message : "Utilisateur créé !"}))
-//         .catch( error => res.status(400).json({error}))
-//     })  
-//     .catch(error => res.status(500).json({error})) 
-// };
 
 exports.signup = (req, res, next) => {
     bcrypt.hash(req.body.password, 10)
@@ -30,34 +21,7 @@ exports.signup = (req, res, next) => {
  
 
 // Verifier  SI un utilisateur existe
-// exports.login = (req, res, next) => {
-//     User.findOne({ email: req.body.email })
-//     .then(user => {
-//         if(user === null) {
-//             res.status(401).json({ message : "Paire identifiant/mot de passe incorrect" })
-//         }
-//         else{
-//             bcrypt.compare(req.body.password, user.password)
-//             .then(valid => {
-//                 if(!valid) {
-//                     res.status(401).json({  message : "Paire identifiant/mot de passe incorrect" })
-//                 }
-//                 else{
-//                     res.status(200).json( {
-//                         userId : user._id,
-//                         token: 'TOKEN'
-//                     } )
-//                 }
-//             })
-//             .then(error => {
-//                 res.status(500).json( {error} );
-//             })
-//         }
-//     })
-//     .catch(error => {
-//         res.status(500).json( {error} )
-//     })
-//  };
+
 exports.login = (req, res, next) => {
     User.findOne({ email: req.body.email })
         .then(user => {
@@ -71,27 +35,25 @@ exports.login = (req, res, next) => {
                     }
                     res.status(200).json({
                         userId: user._id,
-                        token: 'TOKEN'
+                        token: jwt.sign(
+                            //Le donnees qu'on veut encoder
+                            {userId: user._id},
+                            //Cles secre d'encodage
+                            'RANDOM_TOKEN_SECRET',
+                            //Configuration. chaque token durera 24h
+                            { expireIn: '24' }
+                        )
                     });
                 })
                 .catch(error => res.status(500).json({ error }));
         })
         .catch(error => res.status(500).json({ error }));
  };
- 
 
-//  exports.getAllThing = (req, res, next) => {
-//     delete req.body._id; 
-//     const thing = new Thing({...req.body})
-//     thing.save()
-//     .then(res.status(201).json({message : "Objet enregistré !"})) 
-//     .catch(error => res.status(404).json(error))
-// }
-
-//  exports.getAllUsers = (req, res, next) => {
-//     delete req.body._id;
-//     const user = new User({ ...req.body})
-//     user.save()
-//     .then(res.status(201).json({user})) 
-//     .catch(error => res.status(404).json(error))
-//  }
+ exports.getAllUsers = (req, res, next) => {
+    delete req.body._id;
+    const user = new User({ ...req.body})
+    user.save()
+    .then(res.status(201).json({user})) 
+    .catch(error => res.status(404).json(error))
+ }
