63f4b744815a7974acadedf28e8937b59d3aed24
jeannot diambu <diambujeannot@gmail.com>
2022-10-14 12:26:45 +0100

    Creation des USER end Login

--- controllers/user.js
+++ controllers/user.js
@@ -2,23 +2,96 @@
 const bcrypt = require('bcrypt')
 const User = require('../models/User')
 
-//Installation de bcrypt // npm install --save bcrypt
+//Enregistrement nouveau utilisateur
+// exports.signup = (req, res, next) => {
+//     bcrypt.hash(req.body.password, 10)
+//     .then( hash => { 
+//         const user = new User ({ email: req.body.email, password: hash});
+//         user.save()
+//         .then(() => res.status(201).json({message : "Utilisateur créé !"}))
+//         .catch( error => res.status(400).json({error}))
+//     })  
+//     .catch(error => res.status(500).json({error})) 
+// };
 
 exports.signup = (req, res, next) => {
-    //hasher le mot de passe
     bcrypt.hash(req.body.password, 10)
-    .then( hash => { // Recuprer le hash de mot de passe et on l'enregistre dans un user 
-        const user = new User ({ email: req.body.email, password: hash /* hash cree qu'on enregistre */});
+      .then(hash => {
+        const user = new User({
+          email: req.body.email,
+          password: hash
+        });
         user.save()
-        .then(() => res.status(201).json({message : "Utilisateur créé !"}))
-        .catch( error => res.status(400).json(error))
-    })  // Ici on recupere le hash de mot de passe
-    .catch(error => res.stat(500).json(error)) // 500 erreur serveur
-    // combien de fois on execute l'algorithme de hasher => 10 tour
-};
-
-
-exports.login = (req, res, next) => {
-
-};
+          .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
+          .catch(error => res.status(400).json({ error }));
+      })
+      .catch(error => res.status(500).json({ error }));
+  };
  
+
+// Verifier  SI un utilisateur existe
+// exports.login = (req, res, next) => {
+//     User.findOne({ email: req.body.email })
+//     .then(user => {
+//         if(user === null) {
+//             res.status(401).json({ message : "Paire identifiant/mot de passe incorrect" })
+//         }
+//         else{
+//             bcrypt.compare(req.body.password, user.password)
+//             .then(valid => {
+//                 if(!valid) {
+//                     res.status(401).json({  message : "Paire identifiant/mot de passe incorrect" })
+//                 }
+//                 else{
+//                     res.status(200).json( {
+//                         userId : user._id,
+//                         token: 'TOKEN'
+//                     } )
+//                 }
+//             })
+//             .then(error => {
+//                 res.status(500).json( {error} );
+//             })
+//         }
+//     })
+//     .catch(error => {
+//         res.status(500).json( {error} )
+//     })
+//  };
+exports.login = (req, res, next) => {
+    User.findOne({ email: req.body.email })
+        .then(user => {
+            if (!user) {
+                return res.status(401).json({ message: 'Paire login/mot de passe incorrecte'});
+            }
+            bcrypt.compare(req.body.password, user.password)
+                .then(valid => {
+                    if (!valid) {
+                        return res.status(401).json({ message: 'Paire login/mot de passe incorrecte' });
+                    }
+                    res.status(200).json({
+                        userId: user._id,
+                        token: 'TOKEN'
+                    });
+                })
+                .catch(error => res.status(500).json({ error }));
+        })
+        .catch(error => res.status(500).json({ error }));
+ };
+ 
+
+//  exports.getAllThing = (req, res, next) => {
+//     delete req.body._id; 
+//     const thing = new Thing({...req.body})
+//     thing.save()
+//     .then(res.status(201).json({message : "Objet enregistré !"})) 
+//     .catch(error => res.status(404).json(error))
+// }
+
+//  exports.getAllUsers = (req, res, next) => {
+//     delete req.body._id;
+//     const user = new User({ ...req.body})
+//     user.save()
+//     .then(res.status(201).json({user})) 
+//     .catch(error => res.status(404).json(error))
+//  }
