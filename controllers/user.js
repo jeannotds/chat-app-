739b1028299b881523b5ed8d19e5fa3f84a6bbec
jeannot diambu <diambujeannot@gmail.com>
2022-10-17 11:40:06 +0100

    modif1 login

--- controllers/user.js
+++ controllers/user.js
@@ -18,22 +18,28 @@ exports.signup = (req, res, next) => {
             { 
                 message: 'Utilisateur créé !' ,
                 success: 'true',
-                user: {
-                    id: user._id,
-                    username: user.name
-                }
+                user: user
             }
         ))
           .catch(error => res.status(400).json({ error }));
       })
-      .catch(error => res.status(500).json({ error }));
+      .catch(error => res.status(500).json({ 
+
+            message: 'User non créé ! Il y a un probleme !' ,
+            success: 'false',
+            error: error
+
+       }));
   };
 
 exports.login = (req, res, next) => {
     User.findOne({ email: req.body.email })
         .then(user => {
             if (!user) {
-                return res.status(401).json({ error: 'Utilisateur non trouvé !' });
+                return res.status(401).json({ 
+                    success: false,
+                    message: 'Utilisateur non trouvé'
+                 });
             }
             bcrypt.compare(req.body.password, user.password)
                 .then(valid => {
