0f5ad2984d6f452c3c0c9718de8341bec67004b1
jeannot diambu <diambujeannot@gmail.com>
2022-10-18 14:13:35 +0100

    Envois et recuperation du message

--- routes/message.js
+++ routes/message.js
@@ -4,6 +4,6 @@ const { addMessage, getMessages } = require('../controllers/message')
 const router = express.Router()
 
 router.post("/", addMessage)
-// router.get("/:chatId", getMessages)
+router.get("/:chatId", getMessages)
 
 module.exports = router
